a2f63d6ccef9277e1bf6879c1f72517aa52847a0
Alastair Fraser <alastairfraser.work@gmail.com>
2024-06-27 20:22:35 +0200

    number input added

--- task 2/src/components/NumberInput/NumberInput.tsx
+++ task 2/src/components/NumberInput/NumberInput.tsx
@@ -1,18 +1,37 @@
-import './NumberInput.css'
-import { MdKeyboardArrowUp } from "react-icons/md";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import "./NumberInput.css";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
+import { useState } from "react";
 
 export default function NumberInput({ chargePoints, setChargePoints }) {
+  const [inputValue, setInputValue] = useState(chargePoints);
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+    setChargePoints(value === "" ? 0 : Number(value));
+  };
+
+  const increment = () => {
+    const newValue = chargePoints + 1;
+    setInputValue(newValue);
+    setChargePoints(newValue);
+  };
+
+  const decrement = () => {
+    const newValue = chargePoints - 1;
+    setInputValue(newValue);
+    setChargePoints(newValue);
+  };
 
   return (
     <div className="number-wrap">
-      <input type="number" value={chargePoints} onChange={(e)=>setChargePoints(Number(e.target.value))} />
+      <input type="number" value={inputValue} onChange={handleInputChange} />
       <div className="button-wrap">
-        <button onClick={() => setChargePoints((prevCharge) => prevCharge + 1)}>
-          <MdKeyboardArrowUp size={28} color="grey" />
+        <button onClick={increment}>
+          <MdKeyboardArrowUp size={26} color="grey" />
         </button>
-        <button onClick={() => setChargePoints((prevCharge) => prevCharge + 1)}>
-          <MdKeyboardArrowDown size={28} color="grey" />
+        <button onClick={decrement}>
+          <MdKeyboardArrowDown size={26} color="grey" />
         </button>
       </div>
     </div>
